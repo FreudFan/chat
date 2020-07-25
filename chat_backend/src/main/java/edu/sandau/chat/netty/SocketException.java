579e9a9ced9ad8a8d3a910d2551b75f9f3e2c6af
fan.minyang <32455840+FreudFan@users.noreply.github.com>
2020-07-25 17:46:04 +0800

    添加异常日志

--- chat_backend/src/main/java/edu/sandau/chat/netty/SocketException.java
+++ chat_backend/src/main/java/edu/sandau/chat/netty/SocketException.java
@@ -1,9 +1,13 @@
 package edu.sandau.chat.netty;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class SocketException extends RuntimeException {
 
     public SocketException(String message) {
         super(message);
+        log.error(message, this);
     }
 
     public final static String WRONG_FORMAT = "输入格式不正确";
